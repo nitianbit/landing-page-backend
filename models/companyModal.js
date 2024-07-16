809ba22804a762ce636a960bb8f11013790983c9
Tushar Mangla <tushar.mangla1120@gmail.com>
2024-07-16 23:09:06 +0530

    added company model

--- models/companyModal.js
+++ models/companyModal.js
@@ -1,14 +1,9 @@
 import mongoose from "mongoose";
 
 const CompanySchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    companyName: { type: String, required: true },
     websiteURL: { type: String, required: true },
 });
 
-UserSchema.methods.toJSON = function () {
-    const obj = this.toObject();
-    delete obj.password;
-    return obj;
-};
 
 export default mongoose.model("Company", CompanySchema);
