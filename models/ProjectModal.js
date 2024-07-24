b65f16264104b0997d6fd4c22167bba6ca96432a
Tushar Mangla <tushar.mangla1120@gmail.com>
2024-07-25 02:08:26 +0530

    added product section

--- models/ProjectModal.js
+++ models/ProjectModal.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 const projectSchema = new mongoose.Schema({
     name: String,
     description: String,
-    domain: String,
-    companyId: { type: mongoose.Schema.Types.ObjectId, ref: "Company" }
+    domain: { type: String, unique: true },
+    companyId: { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
+    parent: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
 });
 
 const Project = mongoose.model('Project', projectSchema);
