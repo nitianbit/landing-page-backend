7b03810dd8bcb9a6b6912af3dd3af105d619d4f9
Tushar Mangla <tushar.mangla1120@gmail.com>
2024-07-29 22:04:38 +0530

    add form index

--- models/FormModal.js
+++ models/FormModal.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const formSchema = new mongoose.Schema({
     title: String,
+    formIndex: Number,
     fields: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Field' }],
     requiredFields: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Field' }],
     project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' },
