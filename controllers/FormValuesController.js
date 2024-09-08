a4e2a2bac54cd9343820c546a8b271d37e0e68af
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-08 11:37:58 +0530

    projectId added in csv

--- controllers/FormValuesController.js
+++ controllers/FormValuesController.js
@@ -57,11 +57,26 @@ export const getProjectFormValues = async (req, res) => {
     try {
         const { projectId, formId, } = req?.params;
         const {refererId=""}=req.query;
-        const response = await FormValue?.find({
+                const query=FormValue?.find({
             projectId,
             formId,
             ...(refererId && { refererId: { $in: [refererId] } })
-        })
+        });
+        
+        if(req.query.download){
+            query.populate({
+                path: 'projectId',
+                select: 'name'  
+            })
+            .populate({
+                path: 'refererId',
+                select: 'name' 
+            });
+    
+        }
+        
+        const response = await query.lean(true);
+        
         if (req.query.download) {
             const csvData = convertToCsv(response);
             return res.status(200).send({ data: csvData });
