75dfe415140e9d3a3770dae3f4dd4a11c894a02b
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-17 18:26:09 +0530

    Update ProjectModal.js

--- models/ProjectModal.js
+++ models/ProjectModal.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const projectSchema = new mongoose.Schema({
     name: String,
     description: String,
-    domain: { type: String, unique: true },
+    domain: { type: String},
     companyId: { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
     parent: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
     forms: [{ type: mongoose.Schema.Types.ObjectId, ref: "Form" }],//childforms
